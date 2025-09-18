2b8f9c7a1684167283697ccad332b183e1077920
pchchv <ipchchv@gmail.com>
2025-09-18 14:34:42 +0300

    Init

---
+++ bin/rest_dart_prod.dart
@@ -0,0 +1,5 @@
+import 'package:rest_dart_prod/rest_dart_prod.dart' as rest_dart_prod;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rest_dart_prod.calculate()}!');
+}
---
+++ test/rest_dart_prod_test.dart
@@ -0,0 +1,8 @@
+import 'package:rest_dart_prod/rest_dart_prod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
