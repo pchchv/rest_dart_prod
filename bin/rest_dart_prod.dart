9d5dcf7ebc5269222ab5537e84398510e4e887cc
pchchv <ipchchv@gmail.com>
2025-09-19 10:33:32 +0300

    refactor: remove base application

--- bin/rest_dart_prod.dart
+++
@@ -1,5 +0,0 @@
-import 'package:rest_dart_prod/rest_dart_prod.dart' as rest_dart_prod;
-
-void main(List<String> arguments) {
-  print('Hello world: ${rest_dart_prod.calculate()}!');
-}
--- test/rest_dart_prod_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:rest_dart_prod/rest_dart_prod.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
