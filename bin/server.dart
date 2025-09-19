2992d722a3705955c264e902b33f68639b582d99
pchchv <ipchchv@gmail.com>
2025-09-19 12:16:22 +0300

    refactor(server):

--- bin/server.dart
+++ bin/server.dart
@@ -3,11 +3,11 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'package:rest_dart_prod/router.dart';
 
 void main(List<String> args) async {
-  // Configure the pipeline with a logger.
-  final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router.call);
-
   // Configure routes.
   configureRoutes();
+
+  // Configure the pipeline with a logger.
+  final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router.call);
   
   // Start server.
   final server = await io.serve(handler, 'localhost', 8080);
