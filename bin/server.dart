389b929d73b37d3f03240269151920593245d8d8
pchchv <ipchchv@gmail.com>
2025-09-19 11:58:11 +0300

    feat(server): start Shelf server on localhost:8080 with routes

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,15 @@
 import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
 import 'package:rest_dart_prod/router.dart';
 
 void main(List<String> args) async {
   // Configure the pipeline with a logger.
   final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router.call);
+
+  // Configure routes.
+  configureRoutes();
+  
+  // Start server.
+  final server = await io.serve(handler, 'localhost', 8080);
+  print('Server listening on http://${server.address.host}:${server.port}');
 }
